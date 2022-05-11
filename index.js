3b2a2953a160ba6365e013fb3b05ba5580f1dfe2
Ajay Wala <ajay.spyhunteritsolution@gmail.com>
2022-05-11 14:38:41 +0530

    WIP - error hendleing axios.intersept on index file

--- index.js
+++ index.js
@@ -29,6 +29,74 @@ const initializeAxiosData = (url)=>{
   console.log("call and set default url");
  axios.defaults.baseURL = url;
 }
+window.store = store;
+
+const getErrorMessage = (data) => {
+  // check for data is object
+
+  if (data?.message && data?.message != undefined) {
+    return data.message;
+  }
+
+  if (typeof data == 'object' && !Array.isArray(data)) {
+    return getErrorMessage(data[Object.keys(data)[0]]);
+  }
+  //  check for array
+  if (Array.isArray(data)) {
+    return getErrorMessage(
+      data.length > 0 ? data[0] : 'Error message not found.',
+    );
+  }
+  if (typeof data == 'string') {
+    return data;
+  }
+};
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response && error.response.status === 401) {
+      axios.defaults.headers.common['Authorization'] = '';
+      delete axios.defaults.headers.common['Authorization'];
+      // window.store.dispatch(AuthActions.logout());
+      error = {
+        ...error,
+        response: {message: getErrorMessage(error.response.data)},
+      };
+    }
+
+    if (typeof error.response === 'undefined') {
+      error = {...error, response: {message: 'Network error!'}};
+    }
+
+    if (error.response.status === 400) {
+      error = {
+        ...error,
+        response: {
+          ...error.response,
+          message: getErrorMessage(error.response.data),
+        },
+      };
+    }
+
+    if (error.response.status === 403) {
+      error = {
+        ...error,
+        response: {
+          message: "You don't have permission to access this resource.",
+        },
+      };
+    }
+
+    if (error.response.status === 500) {
+      error = {...error, response: {message: 'Internal server error!'}};
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 const persistor = persistStore(store, {}, () => {
   const {user} = store.getState().auth;
