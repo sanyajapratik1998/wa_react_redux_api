24d00412ee05f931426de5890cbc21f4fb07af75
Ajay Wala <ajay.spyhunteritsolution@gmail.com>
2022-05-12 11:53:57 +0530

    WIP - added provider and PersistGate export from lib

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {applyMiddleware, createStore} from 'redux';
-import {persistReducer, persistStore} from 'redux-persist';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import axios from 'axios';
-import rootReducer from './reduxsauce/';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import axios from "axios";
+import rootReducer from "./reduxsauce/";
 // import Constants from 'expo-constants';
 
 // const ENV = Constants.manifest.extra?.ENV;
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage: AsyncStorage,
-  blacklist: ['common', 'order', 'cart'],
-  whitelist: ['auth', 'config', 'promotion', 'recentProducts'],
+  blacklist: ["common", "order", "cart"],
+  whitelist: ["auth", "config", "promotion", "recentProducts"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
   persistedReducer,
-  applyMiddleware(thunk, logger),
+  applyMiddleware(thunk, logger)
   // ['local', 'development'].includes(ENV)
   //   ? applyMiddleware(thunk, logger)
   //   : applyMiddleware(thunk),
 );
 
-const initializeAxiosData = (url)=>{
+const initializeAxiosData = (url) => {
   console.log("call and set default url");
- axios.defaults.baseURL = url;
-}
+  axios.defaults.baseURL = url;
+};
 window.store = store;
 
 const getErrorMessage = (data) => {
@@ -38,16 +40,16 @@ const getErrorMessage = (data) => {
     return data.message;
   }
 
-  if (typeof data == 'object' && !Array.isArray(data)) {
+  if (typeof data == "object" && !Array.isArray(data)) {
     return getErrorMessage(data[Object.keys(data)[0]]);
   }
   //  check for array
   if (Array.isArray(data)) {
     return getErrorMessage(
-      data.length > 0 ? data[0] : 'Error message not found.',
+      data.length > 0 ? data[0] : "Error message not found."
     );
   }
-  if (typeof data == 'string') {
+  if (typeof data == "string") {
     return data;
   }
 };
@@ -58,17 +60,17 @@ axios.interceptors.response.use(
   },
   function (error) {
     if (error.response && error.response.status === 401) {
-      axios.defaults.headers.common['Authorization'] = '';
-      delete axios.defaults.headers.common['Authorization'];
+      axios.defaults.headers.common["Authorization"] = "";
+      delete axios.defaults.headers.common["Authorization"];
       // window.store.dispatch(AuthActions.logout());
       error = {
         ...error,
-        response: {message: getErrorMessage(error.response.data)},
+        response: { message: getErrorMessage(error.response.data) },
       };
     }
 
-    if (typeof error.response === 'undefined') {
-      error = {...error, response: {message: 'Network error!'}};
+    if (typeof error.response === "undefined") {
+      error = { ...error, response: { message: "Network error!" } };
     }
 
     if (error.response.status === 400) {
@@ -91,21 +93,21 @@ axios.interceptors.response.use(
     }
 
     if (error.response.status === 500) {
-      error = {...error, response: {message: 'Internal server error!'}};
+      error = { ...error, response: { message: "Internal server error!" } };
     }
 
     return Promise.reject(error);
-  },
+  }
 );
 
 const persistor = persistStore(store, {}, () => {
-  const {user} = store.getState().auth;
+  const { user } = store.getState().auth;
   if (user?.token) {
-    axios.defaults.headers.common['Authorization'] = `Token ${user.token}`;
+    axios.defaults.headers.common["Authorization"] = `Token ${user.token}`;
   } else {
-    axios.defaults.headers.common['Authorization'] = '';
-    delete axios.defaults.headers.common['Authorization'];
+    axios.defaults.headers.common["Authorization"] = "";
+    delete axios.defaults.headers.common["Authorization"];
   }
 });
 
-export {store, persistor, initializeAxiosData};
+export { Provider, PersistGate, store, persistor, initializeAxiosData };
