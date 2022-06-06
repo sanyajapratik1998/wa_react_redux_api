f848f4e83da82871365e35c834b178a58eaac2c8
Ajay Wala <ajay.spyhunteritsolution@gmail.com>
2022-06-06 14:18:48 +0530

    WIP - added on get Chat list data and send message to server

--- reduxsauceActions/SupportActions.js
+++ reduxsauceActions/SupportActions.js
@@ -23,4 +23,36 @@ const fetchActiveChatData =
       });
   };
 
-module.exports = { fetchActiveChatData };
+const onGetChatData = (id, callback) => async (dispatch, getState) => {
+  dispatch(CommonActions.setLoading(true));
+  try {
+    const response = await axios.get(`/support/chat/list/${id}`);
+    dispatch(CommonActions.setLoading(false));
+    callback && callback("success", response["data"]);
+  } catch (error) {
+    console.log("error -----------", error);
+    dispatch(CommonActions.setLoading(false));
+    dispatch(
+      CommonActions.setAlert({
+        visible: true,
+        content: error["response"]["message"] || "Something went wrong :(",
+      })
+    );
+  }
+};
+const onSendMessageOnSupport =
+  (body, callback) => async (dispatch, getState) => {
+    try {
+      const response = await axios.post("/support/chat/create", body);
+      callback("success", response);
+    } catch (error) {
+      dispatch(
+        CommonActions.setAlert({
+          visible: true,
+          content: error["response"]["message"] || "Something went wrong :(",
+        })
+      );
+      callback("failed", error["response"]["message"]);
+    }
+  };
+module.exports = { fetchActiveChatData, onGetChatData, onSendMessageOnSupport };
