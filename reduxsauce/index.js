ce69fcdc8019b61aa0b1d29c84ce4f10f950fbf0
Ajay Wala <ajay.spyhunteritsolution@gmail.com>
2022-06-29 12:47:21 +0530

    WIP - fix cart after logout

--- reduxsauce/index.js
+++ reduxsauce/index.js
@@ -1,22 +1,22 @@
-const {combineReducers} = require('redux')
-const {resettableReducer} = require('reduxsauce')
-const {AuthTypes} = require('./authRedux')
+const { combineReducers } = require("redux");
+const { resettableReducer } = require("reduxsauce");
+const { AuthTypes } = require("./authRedux");
 // console.log('AuthTypes.LOGOUT',require('./authRedux'));
 const resettable = resettableReducer(AuthTypes.LOGOUT);
 const rootReducer = combineReducers({
-  auth: resettable(require('./authRedux').reducer),
-  common: resettable(require('./commonRedux').reducer),
-  home: resettable(require('./homeRedux').reducer),
-  notification: resettable(require('./notificationRedux').reducer),
-  order: resettable(require('./orderRedux').reducer),
-  products: resettable(require('./productRedux').reducer),
-  recentProducts: resettable(require('./recentProductRedux').reducer),
-  favorite: resettable(require('./favoriteRedux').reducer),
-  services: resettable(require('./servicesRedux').reducer),
-  menu: resettable(require('./menuRedux').reducer),
-  cart: resettable(require('./cartRedux').reducer),
-  promotion: resettable(require('./promotionRedux').reducer),
-  config: require('./configRedux').reducer,
-  franchise: resettable(require('./franchiseRedux').reducer),
+  auth: resettable(require("./authRedux").reducer),
+  common: resettable(require("./commonRedux").reducer),
+  home: resettable(require("./homeRedux").reducer),
+  notification: resettable(require("./notificationRedux").reducer),
+  order: resettable(require("./orderRedux").reducer),
+  products: resettable(require("./productRedux").reducer),
+  recentProducts: resettable(require("./recentProductRedux").reducer),
+  favorite: resettable(require("./favoriteRedux").reducer),
+  services: resettable(require("./servicesRedux").reducer),
+  menu: resettable(require("./menuRedux").reducer),
+  cart: require("./cartRedux").reducer,
+  promotion: resettable(require("./promotionRedux").reducer),
+  config: require("./configRedux").reducer,
+  franchise: resettable(require("./franchiseRedux").reducer),
 });
-module.exports = {rootReducer} 
+module.exports = { rootReducer };
